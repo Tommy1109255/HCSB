cb22880bc3b651c98708c5657d4aa4bbda3b7c64
guomiemie <1109255675@qq.com>
2026-06-03 10:48:00 +0800

    fix: remove generated frontend files from git tracking

--- src/main/frontend/generated/theme-hcbs.global.generated.js
+++
@@ -1,6 +0,0 @@
-// When this file is imported, global styles are automatically applied
-
-import '@vaadin/vaadin-lumo-styles/color-global.js';
-import '@vaadin/vaadin-lumo-styles/typography-global.js';
-import 'themes/hcbs/styles.css';
-
